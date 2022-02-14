5bfcad091758661fa5033589ec4a56ed7b79866c
Zeyneb Bayrak <85934122+zby-zy@users.noreply.github.com>
2022-02-14 13:16:41 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,52 @@
-#include <iosream>
+#include <iostream>
 using namespace std;
 
+void showMenu(){
+    std::cout << "**********MENU********" << std::endl;
+    std::cout << "1. Check Balance" << std::endl;
+    std::cout << "2. Deposit" << std::endl;
+    std::cout << "3. Withdraw" << std::endl;
+    std::cout << "4. Exit" << std::endl;
+    std::cout << "**********************" << std::endl;
+
+}
 
 int main()
 {
-  
+    int option;
+    double balance = 500;
 
+
+    do{
+    showMenu();
+    cout << "Option: ";
+    cin >> option;
+    system("cls");
+
+    switch (option)
+    {
+    case 1:
+        cout << "Balance is: " << balance << "$" << endl;
+        break;
+    case 2:
+        cout << "Deposit amount:";
+        double depositAmount;
+        cin >> depositAmount;
+        balance += depositAmount;
+        break;
+    case 3:
+        cout << "Withdraw amount:";
+        double withdrawAmount;
+        cin >> withdrawAmount;
+        if (withdrawAmount <= balance)
+            balance -= withdrawAmount;
+        else
+            cout << "Not enough money" << endl;
+        break;
+    }
+    }while(option!=4);
+
+
+    system("pause>0");
 
 }
